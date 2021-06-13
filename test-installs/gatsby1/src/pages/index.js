ad7bc5207384b70606d72d36255ee8d29fe0adab
Ben Williams <ben.williams@colorado.edu>
2021-06-12 18:22:30 -0700

    dropped support for gatsby v1

--- test-installs/gatsby1/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { StyleSheet, css } from "aphrodite";
-
-const styles = StyleSheet.create({ message: { color: "red" } });
-
-const IndexPage = () => <div className={css(styles.message)}>Hello World</div>;
-
-export default IndexPage;
